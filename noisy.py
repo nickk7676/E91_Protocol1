5718888b8f5f887454658b59720ff8535544e628
code-iddd <code.id3d@gmail.com>
2026-07-24 19:08:58 +0100

    Implement noisy circuit with run_round function
    Added a noisy circuit implementation with parameters for clean, noisy, and eavesdropped modes.

--- noisy.py
+++ noisy.py
@@ -1,4 +1,33 @@
-#will be the the noisy circuit
+import cirq
+import numpy as np
+import random
 
-#def run_noisy():
+num_rounds = 2000
 
+alice_bases = {
+  'a1': 0,
+  'a2': np.pi / 4,
+  'a3': np.pi / 2
+}
+
+bob_bases = {
+  'b1': np.pi / 4,
+  'b2': np.pi / 2,
+  'b3': 3 * np.pi / 4
+}
+
+def run_round(mode="clean", noise_p=0.0, eve_rate=0.0):
+    """
+    :param mode: 'clean', 'noisy', or 'eavesdropped'
+    :param noise_p: Depolarizing noise probability per qubit (for 'noisy' mode)
+    :param eve_rate: Probability of Eve interecepting and measuring (for 'eavesdropped' mode)
+    """
+
+alice_random_base = random.choice(list(alice_bases.keys()))
+alice_theta = alice_bases[alice_random_base]
+
+bob_random_base = random.choice(list(bob_bases.keys()))
+bob_theta = bob_bases[bob_random_base]
+
+qubits = cirq.NamedQubit.range(2, prefix='q')
+circuit = cirq.Circuit()
