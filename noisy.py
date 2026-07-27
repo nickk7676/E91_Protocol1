17c80eee526556a037bf683d8566f9e88293fa42
code-iddd <code.id3d@gmail.com>
2026-07-27 18:30:05 +0100

    Correct syntax and implement noise in quantum circuit
    Fix syntax errors and add noise handling in circuit.

--- noisy.py
+++ noisy.py
@@ -31,3 +31,39 @@ bob_theta = bob_bases[bob_random_base]
 
 qubits = cirq.NamedQubit.range(2, prefix='q')
 circuit = cirq.Circuit()
+
+circuit.append([
+    cirq.H(qubits[0]),
+    cirq.X(qubits[1]),
+    cirq.CNOT(qubits[0], qubits[1]),
+    cirq.Z(qubits[0]),
+])
+
+if mode = "noisy" and noise_p > 0:
+    circuit.append([
+        cirq.deploraize(noise_p).on(qubits[0]),
+        cirq.deploraize(noise_p).or(qubits[1])
+    ])
+
+elif mode = "eavesdropped" and eve_rate > 0
+    if random.random() < eve_rate:
+        eve._basis_theta = random.choice([0, np.pi / 4, np.pi / 2])
+        circuit.append(cirq.rx(eve_basis_theta.on(qubits[1]))
+        circuit.append(cirq.measure(qubits[1], key='eve'))
+        circuit.append(cirq.rx(-eve_basis_theta).on(qubits[1]))
+
+circuit.append(cirq.rx(alice_theta).on(qubits[0]))
+circuit.append(cirq.rx(bob_theta).on(qubits[1]))
+
+circuit.append(cirq.measure(qubits[0], key='alice')
+circuit.append(cirq.measure(qubits[1], key='bob')
+
+sim = cirq.Simulator()
+result = sim.run(circuit)
+
+return {
+      'alice_random_base': alice_random_base,
+      'bob_random_base': bob_random_base,
+      'alice_result': result.measurements['alice'][0][0]
+      'bob_result': result.measurements['bob'][0][0]
+}
